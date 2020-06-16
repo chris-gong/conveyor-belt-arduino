f57322298f4c860f4fe3a1527150662643c975df
BEASTSM96 <49040508+BEASTSM96@users.noreply.github.com>
2020-06-16 22:23:55 +0100

    Revmoved Bad Code

--- ConveyorBelt.ino
+++ ConveyorBelt.ino
@@ -1,39 +1,13 @@
 const int stepPin = 3; 
 const int dirPin = 4; 
 
-//const int highButtonPin = 5;
-//const int lowButtonPin = 6;
- 
 void setup() {
-  // Sets the two pins as Outputs
+  
   pinMode(stepPin, OUTPUT); 
   pinMode(dirPin, OUTPUT);
-  //pinMode(lowButtonPin, INPUT);
-  //pinMode(highButtonPin, INPUT);
+  
 }
 void loop() {
-
-  /*int lowButtonState, highButtonState = 0;
-
-  lowButtonState = digitalRead(lowButtonPin);
-  highButtonState = digitalRead(highButtonPin);
-  Serial.println(lowButtonState);
-  if (lowButtonState == LOW || highButtonState == LOW) {
-    if (lowButtonState == HIGH) {
-      digitalWrite(dirPin, LOW);
-      digitalWrite(stepPin, HIGH); 
-      delay(1);
-      digitalWrite(stepPin, LOW); 
-      delay(1);
-    }
-    else {
-      digitalWrite(dirPin, HIGH);
-      digitalWrite(stepPin, HIGH); 
-      delay(1);
-      digitalWrite(stepPin, LOW); 
-      delay(1);
-    }
-  }*/
   digitalWrite(dirPin, HIGH);
   for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin, HIGH); 
